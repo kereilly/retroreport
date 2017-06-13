337197938c5dc9a5f6677c06e340de986c1da389
kereilly <kreilly@retroreport.com>
2017-06-13 18:42:55 -0400

    more good coding

--- emam_archival.py
+++ emam_archival.py
@@ -488,7 +488,7 @@ def post_download(args, job, rough_screener_path, v=1):
     return job
 
 
-def emam_metadata_format(jobs):
+def emam_metadata_format(jobs, category):
 
     list_assets = []
     for job in jobs:
@@ -497,49 +497,70 @@ def emam_metadata_format(jobs):
         asset.title = job['file_name']
         asset.description = job['description']
         asset.file_name = job['file_name_ext']
-        asset.file_path = '\\\\10.0.8.5\\d\\videos'
+        asset.file_path = "\\\\10.0.2.8\\xml_ingest"
         asset.file_action = FileAction.MOVE
         asset.ingest_action = IngestAction.CREATE_NEW_ASSET
 
         # Define custom metadata fields.
         custom_metadata = []
         # Mandatory fields
-        metadata = CustomMetadata()
-        metadata.standard_id = 'CUST_FLD_ASSET LABEL_13'
-        metadata.value = job['file_name']
-        custom_metadata.append(metadata)
-        metadata = CustomMetadata()
+        metadata = CustomMetadata()  # Set metadata as CustomMetadata object
+        metadata.standard_id = 'CUST_FLD_ASSET LABEL_13'    # Add the id for metadata field
+        metadata.value = job['file_name']       # Add the value of the feild
+        custom_metadata.append(metadata)    # append metadata to the list
+        metadata = CustomMetadata()     # reset custom metadata object
         metadata.standard_id = 'CUST_FLD_ASSET NUMBER_25'
         metadata.value = job['asset_number']
         custom_metadata.append(metadata)
-        asset.custom_metadata = custom_metadata
+        metadata = CustomMetadata()
         metadata.standard_id = 'CUST_FLD_SOURCE_17'
         metadata.value = job['source']
         custom_metadata.append(metadata)
+        metadata = CustomMetadata()
+        metadata.standard_id = 'CUST_FLD_PROJECT ID_29'
+        metadata.value = job['project_id']
+        custom_metadata.append(metadata)
+
         # Non mandatory fields
         if job['source_id'] != "":
-            asset.custom_metadata = custom_metadata
+            metadata = CustomMetadata()
             metadata.standard_id = 'CUST_FLD_SOURCE ID_18'
             metadata.value = job['source_id']
             custom_metadata.append(metadata)
         if job['description'] != "":
-            asset.custom_metadata = custom_metadata
+            metadata = CustomMetadata()
             metadata.standard_id = 'CUST_FLD_DESCRIPTION_19'
             metadata.value = job['description']
             custom_metadata.append(metadata)
         if job['link'] != "":
-            asset.custom_metadata = custom_metadata
+            metadata = CustomMetadata()
             metadata.standard_id = 'CUST_FLD_LINK_20'
             metadata.value = job['link']
             custom_metadata.append(metadata)
         if job['notes'] != "":
-            asset.custom_metadata = custom_metadata
+            metadata = CustomMetadata()
             metadata.standard_id = 'CUST_FLD_NOTES_32'
             metadata.value = job['notes']
+            custom_metadata.append(metadata)
+        #  Extract date
+        dict_date = job['date']
+        if dict_date['year'] != "":  # use the year as the condition if a date is present
+            metadata = CustomMetadata()
+            metadata.standard_id = 'CUST_FLD_DATE_5'
+            date = dict_date['year']
+            if dict_date['month'] != "":    # test if month is present
+                date = date + "/" + dict_date['month']
+            if dict_date['day'] != "":
+                date = date + "/" + dict_date['month']
+            print date
+            metadata.value = date
             custom_metadata.append(metadata)
 
         # load meta data into asset
         asset.custom_metadata = custom_metadata
+
+        # Add category
+        asset.categories = category
 
         # load asset into list
         list_assets.append(asset)
@@ -583,6 +604,20 @@ def main():
         print ("The Google Sheet JavaScript did not provide a project ID")
         print ("Specify your own project ID with the -r flag. Downloader will not continue")
         exit()
+
+    # Grab the eMAM category for assets
+
+    if csv_dump[0][1] != "" and csv_dump[0][2] != "":
+        category = []
+        category.append(csv_dump[0][2] + "/" + project_id + " " +
+                        csv_dump[0][1] + "/" + csv_dump[0][3])  # the category from the csv file
+    else:
+        print ("No category defined\n Will not go on")
+        print ("You must define a category")
+        exit()
+
+    if verbosity >= 2:
+        print ("Category path is: " + category)
 
     # copy the rest of the list, everything except first element
     after_first_item = False
@@ -658,15 +693,24 @@ def main():
     future_import_jobs = []  # holds the assets for a separate xml file that will be used for batch import in the future
     for job in processed_jobs:
         if job['downloaded']:   # downloaded holds a boolean
-            downloaded_jobs.append(job)
-        else:
-            future_import_jobs.append(job)
+            downloaded_jobs.append(job)  # this job was downloaded
+        else:   # These jobs were not downloaded, add to separate list
+            # Don't add to list if they are vanderbilt assets
+            if job['source'].lower() == "vanderbilt":
+                print ("vandy. not adding")
+            elif job['source'].lower() == "vandy":
+                print ("vandy. not adding")
+            else:
+                future_import_jobs.append(job)
 
     # Get the xml ready for files that we have now
-    downloaded_job_xml_list = emam_metadata_format(downloaded_jobs)
+    downloaded_job_xml_list = emam_metadata_format(downloaded_jobs, category)
 
     # Create our xml file
-    generate_sidecar_xml('5k4gfs78hdf', downloaded_job_xml_list, '/Users/edit6/Desktop/test2.xml')
+    location = location + "sidecar.xml"
+    print location
+    generate_sidecar_xml('DlmCO%2frHfqn8MFWM72c2oEXEdfnMecNFm8Mz413k%2fUzRtOsyTzHvBg%3d%3d', downloaded_job_xml_list,
+                         location)
 
     print ("\nJobs for future import")
     for job in future_import_jobs:
